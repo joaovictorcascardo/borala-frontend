f67aed94accd0860896dcec88e19f2c9871e99f3
Joao Prado <Joaovictorpachecoleite@gmail.com>
2026-05-01 21:31:51 -0300

    fix: resolve import error in main and add basic login route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Home - Borala</h1>} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );

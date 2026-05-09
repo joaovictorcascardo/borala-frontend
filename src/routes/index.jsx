95a0a61c491ff52f88f9e75e82b7dad09b01f724
Joao Prado <Joaovictorpachecoleite@gmail.com>
2026-05-09 16:04:02 -0300

    feat: implement protected routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,16 +3,18 @@ import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import ForgotPasswordPage from "../pages/ForgotPasswordPage";
 import ResetPasswordPage from "../pages/ResetPasswordPage";
-
+import ProtectedRoute from "../components/ProtectedRoute";
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home - Borala</h1>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<h1>Home - Borala</h1>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
